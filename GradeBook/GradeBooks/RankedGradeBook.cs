bbaec5a0df167f3ecfca2fa56bbef46408c9718b
Will Gordon <42868645+vilhelmus@users.noreply.github.com>
2018-08-31 15:08:00 +0100

    Exercise: Override GetLetterGrade
    PluralSight exercise on adding features to grade book application

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,4 @@
+using System;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +8,31 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name):base(name)
         {
            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade <= 20)
+            {
+                return 'A';
+            }
+            if (averageGrade > 20 && averageGrade <= 40)
+            {
+                return 'B';
+            }
+            if (averageGrade > 40 && averageGrade <= 60)
+            {
+                return 'C';
+            }
+            if (averageGrade > 60 && averageGrade <= 80)
+            {
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
